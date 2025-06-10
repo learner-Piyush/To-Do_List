52f213328cc404271871feab5ab4d7a8afa0fffd
learner-Piyush <sciencetechoindia@gmail.com>
2025-06-10 19:47:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addButton = document.getElementById("add-button");
+  const todoList = document.getElementById("todo-list");
+
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks.forEach((task) => renderTasks(task));
+
+  addButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") {
+      return;
+    }
+
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+
+    tasks.push(newTask);
+    saveTasks();
+    renderTasks(newTask);
+    todoInput.value = "";
+    console.log(tasks);
+  });
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  function renderTasks(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) {
+      li.classList.add("completed");
+    }
+
+    li.innerHTML = `<span>${task.text}</span>
+    <button>Delete
+    <img src="../Image Folder/icons8-delete-48.png" alt="">
+    </button>`;
+    todoList.appendChild(li);
+
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") {
+        return;
+      }
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation();
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    });
+  }
+});
